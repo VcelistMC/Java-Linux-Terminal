2ae12bbd63de31f60a20b6f23519c2402fedf47e
Vcelist <petoilluis@gmail.com>
2021-11-06 22:31:21 +0200

    implemented cd and helper functions

--- src/Terminal.java
+++ src/Terminal.java
@@ -1,10 +1,35 @@
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.Scanner;
+
 class Parser {
     String commandName;
     String[] args;
-    
+
+    public void waitForInput(){
+        String input;
+        Scanner scan = new Scanner(System.in);
+        input = scan.nextLine();
+        parse(input);
+    }
 
     public boolean parse(String input){
-        return false;
+        String[] cmd = input.split(" ");
+        commandName = cmd[0];
+
+        if(cmd.length > 1){
+            if(cmd[1].contains("-")){ 
+                commandName += (" " + cmd[1]);
+                args = Arrays.copyOfRange(cmd, 2, cmd.length);
+            }
+            else{
+                args = Arrays.copyOfRange(cmd, 1, cmd.length);
+            }
+        }
+        return true;
     }
 
     public String getCommandName(){
@@ -18,27 +43,35 @@ class Parser {
 
 public class Terminal {
     Parser parser;
+    Path currentDir, homeDir;
+
+    Terminal(){
+        parser = new Parser();
+        //gets the dir the program was launched in
+        currentDir = Paths.get(System.getProperty("user.dir"));
+        homeDir = Paths.get(System.getProperty("user.home"));
+    }
 
     /**
      * Takes 1 arguments and prints it
      */
-    public String echo(String[] args){}
+    public String echo(String[] args){ return ""; }
 
     /**
      * prints current working directory
      */
-    public String pwd(){}
+    public String pwd(){ return currentDir.toString(); }
 
     
     /**
      * "ls" lists contents of current working dir
      */
-    public String[] ls(String[] args){}
+    public String[] ls(String[] args){ String[] arr = new String[]{}; return arr; }
     
     /**
      * "ls -r" lists contents of current working dir in reverse order
      */
-    public String[] lsr(String[] args){}
+    public String[] lsr(String[] args){ String[] arr = new String[]{}; return arr; }
     
     /**
      * "mkdir [dir name]" creates a folder in current working dir
@@ -63,7 +96,25 @@ public class Terminal {
      * <p>
      * "cd [abs path] or [relative path]" changes current working dir to path in arg
      */
-    public void cd(String[] args){}
+    public void cd(String[] args) throws Exception{
+        // code can access zip files lol????????
+        if(args == null)
+            currentDir = homeDir;
+        else{
+            if(args[0].equals(".."))
+                currentDir = currentDir.getParent();
+            else{
+                Path tmpPath = currentDir.resolve(String.join(" ", args));
+                if(!exists(tmpPath))
+                    throw new Exception("ERROR: Directory not found");
+                if(!isDir(tmpPath))
+                    throw new Exception("ERROR: "+tmpPath+ " is not a directory");
+                currentDir = currentDir.resolve(tmpPath);
+
+            }
+        }
+    }
+
     
     /**
      * "touch [path]" creates a file in selected path
@@ -80,7 +131,7 @@ public class Terminal {
      * <p>
      * "cat [file1] [file2]" prints file1 and file2 contents
      */
-    public String cat(String[] args){}
+    public String cat(String[] args){return ""; }
     
     /**
      * "cp [file1] [file2]" copies contents of file 1 into file 2
@@ -91,4 +142,45 @@ public class Terminal {
      * "cp -r [dir1] [dir2]" copies contents of dir1 into dir2
      */
     public void cpr(String[] args){}
+
+    public void chooseCommandAction(){
+        parser.waitForInput();
+        try{
+            switch (parser.commandName){
+                case "cd":
+                    cd(parser.args);
+                    break;
+            
+                default:
+                    break;
+            }
+        }catch(Exception e){
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public void run(){
+        boolean userExited = false;
+        while(!userExited){
+            System.out.print(pwd()+">");
+            chooseCommandAction();
+        }
+    }
+
+    private boolean isFile(Path path){
+        return !Files.isDirectory(path);
+    }
+
+    private boolean isDir(Path path){
+        return Files.isDirectory(path);
+    }
+    
+    private boolean exists(Path path){
+        return Files.exists(path);
+    }
+
+    public static void main(String[] args) {
+        Terminal term = new Terminal();
+        term.run();
+    }
 }
