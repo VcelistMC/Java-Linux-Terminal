74b46558fbc76a4df5ba9853243a48114e1db713
AbdelrahmanAli-gif <abdelrahmanali58@gmail.com>
2021-11-07 15:58:46 +0200

    Finished echo, ls, ls -r, mkdir, rmdir

--- src/Terminal.java
+++ src/Terminal.java
@@ -2,6 +2,7 @@ import java.io.File;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
 
@@ -21,7 +22,7 @@ class Parser {
         commandName = cmd[0];
 
         if(cmd.length > 1){
-            if(cmd[1].contains("-")){ 
+            if(cmd[1].contains("-")){
                 commandName += (" " + cmd[1]);
                 args = Arrays.copyOfRange(cmd, 2, cmd.length);
             }
@@ -55,31 +56,69 @@ public class Terminal {
     /**
      * Takes 1 arguments and prints it
      */
-    public String echo(String[] args){ return ""; }
+    public String echo(String[] args){
+        String str = "";
+        for (int i = 0; i < args.length; i++)
+            str += args[i] + " ";
+        return str;
+    }
 
     /**
      * prints current working directory
      */
     public String pwd(){ return currentDir.toString(); }
 
-    
+
     /**
      * "ls" lists contents of current working dir
      */
-    public String[] ls(String[] args){ String[] arr = new String[]{}; return arr; }
-    
+    public String ls(String[] args){
+        ArrayList<String> content = new ArrayList<>();
+        File f = new File(String.valueOf(currentDir));
+        ArrayList<File> files = new ArrayList<File>(Arrays.asList(f.listFiles()));
+        for (int i = 0; i < files.size(); i++)
+            content.add(files.get(i).getName());
+        return String.join(" ", content);
+    }
+
     /**
      * "ls -r" lists contents of current working dir in reverse order
      */
-    public String[] lsr(String[] args){ String[] arr = new String[]{}; return arr; }
-    
+    public String lsr(String[] args){
+        ArrayList<String> content = new ArrayList<>();
+        File f = new File(String.valueOf(currentDir));
+        ArrayList<File> files = new ArrayList<File>(Arrays.asList(f.listFiles()));
+        for (int i = files.size() - 1; i >= 0; i--)
+            content.add(files.get(i).getName());
+        return String.join(" ", content);
+    }
+
     /**
      * "mkdir [dir name]" creates a folder in current working dir
      * <p>
      * "mdkir [path]" creates a folder in selected path
      */
-    public void mkdir(String[] args){}
-    
+
+    // Path must be without spaces
+    public void mkdir(String[] args){
+        for (int i = 0; i < args.length; i++){
+            if (!args[i].contains("\\")) {
+                File file = new File(currentDir.toString() + "\\" + args[i]);
+                if(file.mkdir())
+                    System.out.println("Done");
+                else
+                    System.out.println("None");
+            }
+            else {
+                File file = new File(args[i]);
+                if(file.mkdir())
+                    System.out.println("Done");
+                else
+                    System.out.println("None");
+            }
+        }
+    }
+
     /**
      * WARNING: ONLY REMOVES EMPTY DIRS
      * <p>
@@ -87,8 +126,34 @@ public class Terminal {
      * <p>
      * "rmdir [path]" removes dir at selected path iff the dir is empty
      */
-    public void rmdir(String[] args){}
-    
+    public void rmdir(String[] args){
+        if (args[0].equals("*")) {
+            File f = new File(String.valueOf(currentDir));
+            ArrayList<File> files = new ArrayList<File>(Arrays.asList(f.listFiles()));
+            for (int i = 0; i < files.size(); i++) {
+                f = files.get(i);
+                if(f.delete())
+                    System.out.println("Done");
+                else
+                    System.out.println("None");
+            }
+        }
+        else if (!args[0].contains("\\")){
+            File file = new File(currentDir.toString() + "\\" + args[0]);
+            if(file.delete())
+                System.out.println("Done");
+            else
+                System.out.println("None");
+        }
+        else {
+            File file = new File(args[0]);
+            if(file.delete())
+                System.out.println("Done");
+            else
+                System.out.println("None");
+        }
+    }
+
     /**
      * "cd" with no args returns to home dir of user
      * <p>
@@ -115,29 +180,29 @@ public class Terminal {
         }
     }
 
-    
+
     /**
      * "touch [path]" creates a file in selected path
      */
     public void touch(String[] args){}
-    
+
     /**
      * "rm [file]" deletes a file in current working dir
      */
     public void rm(String[] args){}
-    
+
     /**
      * "cat [file]" prints the file contents
      * <p>
      * "cat [file1] [file2]" prints file1 and file2 contents
      */
     public String cat(String[] args){return ""; }
-    
+
     /**
      * "cp [file1] [file2]" copies contents of file 1 into file 2
      */
     public void cp(String[] args){}
-    
+
     /**
      * "cp -r [dir1] [dir2]" copies contents of dir1 into dir2
      */
@@ -150,7 +215,21 @@ public class Terminal {
                 case "cd":
                     cd(parser.args);
                     break;
-            
+                case "echo":
+                    System.out.println(echo(parser.args));
+                    break;
+                case "ls":
+                    System.out.println(ls(parser.args));
+                    break;
+                case "ls -r":
+                    System.out.println(lsr(parser.args));
+                    break;
+                case "mkdir":
+                    mkdir(parser.args);
+                    break;
+                case "rmdir":
+                    rmdir(parser.args);
+                    break;
                 default:
                     break;
             }
@@ -174,7 +253,7 @@ public class Terminal {
     private boolean isDir(Path path){
         return Files.isDirectory(path);
     }
-    
+
     private boolean exists(Path path){
         return Files.exists(path);
     }
